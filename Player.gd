8f70459e8b7872fffb736c8edff6eb6a18cb5be3
Aaron Kummer <aaronjkummer@gmail.com>
2023-04-13 18:01:39 -0400

    before the disaster

--- Player.gd
+++ Player.gd
@@ -1,11 +1,25 @@
 extends CharacterBody2D
 
+@onready var animation_player: AnimationPlayer = get_node("AnimationPlayer")
+@onready var sprite: Sprite2D = get_node("Sprite2D")
+
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+func update_animation():
+	if velocity.x > 0: # Moving right
+		sprite.flip_h = false
+		animation_player.play("walk-right")
+	elif velocity.x < 0: # Moving left
+		sprite.flip_h = true
+		animation_player.play("walk-right")
+	else:
+		animation_player.stop() # Stop the animation if not moving
+
 
 
 func _physics_process(delta):
@@ -24,5 +38,6 @@ func _physics_process(delta):
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	
+	update_animation()
 	move_and_slide()
